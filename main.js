cfffee864f3328daf0454426f9dfd2b5d3653341
Jay Jayabose <jayabose@gmail.com>
2022-03-19 15:26:50 -0400

    tests passed

---
+++ main.js
@@ -0,0 +1,6 @@
+
+document.querySelector("li").remove();
+document.querySelector("span").remove();
+document.querySelectorAll("article")[3].remove();
+document.querySelectorAll("h3")[0].remove();
+document.querySelectorAll("h3")[0].remove();
